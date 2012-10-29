54be9597b679f305a15725ce489a1e6e713284b5
Rafael R. Costa <phaelrlz@gmail.com>
2012-10-29 14:01:10 -0200

    Alteraoes nas tabelas e chaves Estrangeiras.

--- TrabalhoFinal.sql
+++ TrabalhoFinal.sql
@@ -1,6 +1,3 @@
-USE master
-GO
-
 DROP DATABASE Consultorio
 GO
 
@@ -25,6 +22,9 @@ GO
 SP_HELPDB Consultorio
 GO
 
+USE Consultorio
+GO
+
 CREATE TABLE tb_clinicas
 (
 	email varchar(50),
@@ -38,7 +38,7 @@ CREATE TABLE tb_clinicas
 	nome varchar(50) NOT NULL,
 	bairro varchar(50),
 	numero int,
-	id_cidade int identity
+	id_cidade int
 )
 GO
 
@@ -57,11 +57,9 @@ CREATE TABLE tb_funcionarios
 	data_de_cadastro datetime NOT NULL,
 	login varchar(15) NOT NULL,
 	senha varchar(10) NOT NULL,
-	id_cidade int identity,
-	id_clinica int identity,
-	id_cargo int identity,
-
-	FOREIGN KEY(id_clinica) REFERENCES tb_clinicas (id_clinica)
+	id_cidade int,
+	id_clinica int,
+	id_cargo int,
 )
 GO
 
@@ -75,10 +73,8 @@ GO
 CREATE TABLE tb_cidades 
 (
 	id_cidade int identity PRIMARY KEY,
-	nome varchar(50) NOT NULL,
-	id_uf int identity,
-	
-	FOREIGN KEY(id_uf) REFERENCES tb_estados (id_uf)
+	nome varchar(50)  NOT NULL  ,
+	id_uf varchar 
 )
 GO
 
@@ -91,33 +87,7 @@ CREATE TABLE  tb_locais_enc
 	numero int,
 	id_local_enc int identity PRIMARY KEY,
 	endereço varchar(50) NOT NULL,
-	id_cidade int identity ,
-	
-	FOREIGN KEY(id_cidade) REFERENCES tb_cidades (id_cidade)
-)
-GO
-
-CREATE TABLE tb_pacientes 
-(
-	id_paciente int identity PRIMARY KEY,
-	numero int,
-	cidade varchar(50),
-	nome_pai varchar(50),
-	endereço varchar(50),
-	rg varchar(15),
-	nr_convenio varchar(30),
-	nome_mae varchar(50),
-	data_de_cadastro datetime,
-	bairro varchar(50),
-	celular varchar(20),
-	Nome varchar(50),
-	telefone varchar(20),
-	cep varchar(12),
-	cpf varchar(18),
-	id_convenio int identity,
-	id_cidade int identity,
-	
-	FOREIGN KEY(id_cidade) REFERENCES tb_cidades (id_cidade)
+	id_cidade int
 )
 GO
 
@@ -126,11 +96,9 @@ CREATE TABLE tb_consultas
 	id_consulta int identity PRIMARY KEY,
 	data_consulta datetime,
 	horario datetime,
-	id_paciente int identity,
-	id_convenio int identity,
-	id_medico int identity,
-	
-	FOREIGN KEY(id_paciente) REFERENCES tb_pacientes (id_paciente)
+	id_paciente int,
+	id_convenio int,
+	id_medico int
 )
 GO
 
@@ -139,8 +107,8 @@ CREATE TABLE tb_receitas
 	Observações varchar(80),
 	data_receita datetime,
 	id_receita int identity PRIMARY KEY,
-	id_medico int identity,
-	id_medicamento int identity
+	id_medico int,
+	id_medicamento int
 )
 GO
 
@@ -169,16 +137,37 @@ CREATE TABLE tb_convenios
 )
 GO
 
+CREATE TABLE tb_pacientes 
+(
+	id_paciente int PRIMARY KEY,
+	numero int,	
+	cidade varchar(50),
+	nome_pai varchar(50),
+	endereço varchar(50),
+	rg varchar(15),
+	nr_convenio varchar(30),
+	nome_mae varchar(50),
+	data_de_cadastro datetime,
+	data_de_nascimento datetime,
+	bairro varchar(50),
+	celular varchar(20),
+	nome varchar(50),
+	telefone varchar(20),
+	cep varchar(12),
+	cpf varchar(18),
+	id_convenio int,
+	id_cidade int,
+)
+GO
+
 CREATE TABLE tb_laudos 
 (
 	desc_laudo varchar(80),
 	horario_laudo datetime,
 	data_laudo datetime,
 	id_laudo int identity PRIMARY KEY,
-	id_paciente int identity,
-	id_medico int identity,
-	
-	FOREIGN KEY(id_paciente) REFERENCES tb_pacientes (id_paciente)
+	id_paciente int,
+	id_medico int 
 )
 GO
 
@@ -200,9 +189,7 @@ CREATE TABLE tb_medicos
 	Especialidade varchar(50),
 	email varchar(50),
 	crm varchar(50),
-	id_cidade int identity,
-	
-	FOREIGN KEY(id_cidade) REFERENCES tb_cidades (id_cidade)
+	id_cidade int
 )
 GO
 
@@ -215,4 +202,6 @@ ALTER TABLE tb_consultas ADD FOREIGN KEY(id_medico) REFERENCES tb_medicos (id_me
 ALTER TABLE tb_receitas ADD FOREIGN KEY(id_medico) REFERENCES tb_medicos (id_medico)
 ALTER TABLE tb_receitas ADD FOREIGN KEY(id_medicamento) REFERENCES tb_medicamentos (id_medicamento)
 ALTER TABLE tb_laudos ADD FOREIGN KEY(id_medico) REFERENCES tb_medicos (id_medico)
+
+
 
